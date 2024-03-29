a6d7e1b61080ea55969d263a74f12a45c1e2a368
mohamed <mohammedxp2020@gmail.com>
2024-03-29 03:15:05 +0200

    New tutorial and exercises are added

---
+++ exercises/04_loops_exercise/script.js
@@ -0,0 +1,77 @@
+// Loops Exercises
+
+// Exercise: Repetitive tasks with loops
+
+// Task-1
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.log('Counting completed!')
+
+for (var i = 1; i <= 5; i++) {
+    console.log(i);
+}
+console.log('Counting completed!');
+
+
+// Task-2
+console.log(5)
+console.log(4)
+console.log(3)
+console.log(2)
+console.log(1)
+console.log('Countdown finished!')
+
+for (var i = 5; i >= 1; i--) {
+    console.log(i);
+}
+console.log('Countdown finished!');
+
+
+// Task-3
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.log('Counting completed!')
+
+var counter = 1;
+while (counter <= 5) {
+    console.log(counter);
+    counter = counter + 1;
+}
+console.log('Counting completed!');
+
+
+
+// Task-4
+console.log(5)
+console.log(4)
+console.log(3)
+console.log(2)
+console.log(1)
+console.log('Countdown finished!')
+
+var counter = 5;
+while (counter > 0) {
+    console.log(counter);
+    counter = counter - 1;
+}
+console.log('Countdown finished!');
+
+
+// Task-5
+console.log(2018)
+console.log(2019)
+console.log(2020)
+console.log(2021)
+console.log(2022)
+
+var year = 2018;
+while (year <= 2022) {
+    console.log(year);
+    year = year + 1;
+}
---
+++ tutorials/06_loops_lesson/script.js
@@ -0,0 +1,26 @@
+// ===== For loop condition =====
+
+// Happy new year 
+for (i = 10; i >= 0; i--) {
+    console.log(i);
+}
+console.log("Happy New Year!");
+
+// Go for loop
+for (i = 1; i <= 3; i++) {
+    console.log(i);
+}
+console.log("Go");
+
+
+// ===== While loop condition =====
+var counter = 3;
+while (counter > 0) {
+    console.log(counter);
+    counter = counter - 1;
+}
+console.log("Run")
+
+
+
+
